80e1d1c29bf02176860d51d6207979b8189d28eb
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2020-08-04 18:08:58 -0300

    cors test

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php header( 'Location: /public/index.html' ) ;  ?>
+<?php
+header( 'Location: /public/index.html' ) ;
+header("Access-Control-Allow-Origin:*");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type");
+?>
